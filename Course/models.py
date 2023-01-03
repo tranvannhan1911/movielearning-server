29b51de4fc99e05ae2d1911f3b695f82f76bde09
SiZedYi <91822460+SiZedYi@users.noreply.github.com>
2023-01-03 12:31:36 +0700

    Delete Course directory

--- Course/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class course(models.Model) :
-    subject = models.CharField(max_length=255, blank=True)
-
-class lesson(models.Model) :
-    subject = models.CharField(max_length=255, blank=True)
