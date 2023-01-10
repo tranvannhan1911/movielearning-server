9023e617f25afcf4f687a931cb3b75f5ff5d5911
SiZedYi <91822460+SiZedYi@users.noreply.github.com>
2023-01-10 20:27:07 +0700

    Update settings.py

--- movie_learning/settings.py
+++ movie_learning/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf-yasg',
     'rest_framework',
     'corsheaders',
     'core_movie',
