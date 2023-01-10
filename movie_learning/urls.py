313dbb599ca88ab34bd71f5c51483ef5395aa70c
SiZedYi <91822460+SiZedYi@users.noreply.github.com>
2023-01-10 20:14:39 +0700

    Update urls.py

--- movie_learning/urls.py
+++ movie_learning/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Products API",
+      default_version='v1',
+      description="Description",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/',include('core_movie.urls'))
+    path('movie/',include('core_movie.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
