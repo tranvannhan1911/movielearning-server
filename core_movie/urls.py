e55b20616a67f5c3201b7d728745d3648dfad837
SiZedYi <91822460+SiZedYi@users.noreply.github.com>
2023-01-10 15:00:12 +0700

    Update urls.py

--- core_movie/urls.py
+++ core_movie/urls.py
@@ -4,5 +4,7 @@ from core_movie.api import movie
 
 urlpatterns = [
     path('',movie.all_movie,name='all_movie_api'),
-    path('<str:movie_id>/',movie.movie_detail,name='movie_detail_api')
+    path('<str:movie_id>/',movie.movie_detail,name='movie_detail_api'),
+    path('<str:movie_id>/subtitle/',movie.movie_subs,name='movie_subs_api')
+
 ]
